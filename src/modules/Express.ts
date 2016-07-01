eccb95e99428af0c934918624a5618b393d66741
Frederic Nieto <frederic.artus.nieto@gmail.com>
2016-07-01 17:53:14 +0200

    added route generation to express module

--- src/modules/Express.ts
+++ src/modules/Express.ts
@@ -4,15 +4,32 @@
 import * as express from "express";
 import ModuleGroup from "../utils/ModuleGroup";
 import Module from "../utils/Module";
+import {Server} from "http";
+import {Router} from "express";
 
 class ExpressModule extends Module {
     app: express.Application;
-    port: number;
+    server: Server;
+    private routers: {[s: string]: express.Router};
     constructor(moduleGroup: ModuleGroup) {
         super(moduleGroup);
         this.app = express();
-        this.port = process.env.PORT || 8080;
-        this.app.listen(this.port);
+        let port = process.env.PORT || 8080;
+        this.server = this.app.listen(port, function () {
+            var host:string = this.server.address().address;
+            var port:number = this.server.address().port;
+            console.log('Server listening at http://%s:%s', host, port);
+        });
+    }
+
+    getRouter(name: string): Router {
+        let router = this.routers[name];
+        if (router != null) {
+            return router;
+        }
+        router = this.routers[name] = express.Router();
+        this.app.use(name, router);
+        return router;
     }
 }
 export default ExpressModule;
